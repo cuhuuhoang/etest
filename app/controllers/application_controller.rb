9029503b7fbadf6007cb97508851a15fb17e2912
Cu Huu Hoang <huuhoangcu@gmail.com>
2016-04-03 11:57:51 +0700

    working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
     protected
 
@@ -12,6 +13,14 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:full_name, :avatar, :skype, :facebook, :phone,
                                        :address, :dob, :password, :password_confirmation, :current_password) }
+    end
+
+    def layout_by_resource
+      if devise_controller?
+        "devise_layout"
+      else
+        "application"
+      end
     end
 
 end
