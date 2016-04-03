e18d8d329e165b46d95e7d6668c10f0a49bc36e9
Cu Huu Hoang <huuhoangcu@gmail.com>
2016-04-03 15:24:31 +0700

    working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def display_key(key = '')
+    if(key == "error" || key == "alert")
+      "danger"
+    elsif (key == "notice")
+      "warning";
+    else
+      key;
+    end
+  end
 end
