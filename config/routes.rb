ce69475a3a9d0785eaa8081eb8b3c2a9fa267302
Cu Huu Hoang <huuhoangcu@gmail.com>
2016-04-04 16:10:14 +0700

    working

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
   devise_scope :member do
     root :to => 'devise/registrations#new'
-    match '/settings' => 'registrations#settings', as: :settings
+    get '/settings' => 'registrations#settings', as: :settings
   end
 
   root 'static_pages#home'
