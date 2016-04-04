5355f3f8d26072a707214b76cee65a3d2aa68515
Cu Huu Hoang <huuhoangcu@gmail.com>
2016-04-04 16:44:24 +0700

    working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  params[:user].permit(:full_name, :avatar, :skype, :facebook, :phone, :address, :dob)
 
   def settings
     @user = current_user
@@ -8,6 +9,12 @@ class RegistrationsController < Devise::RegistrationsController
     else
       render file: 'public/404', status: 404, formats: [:html]
     end
+  end
+
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
 
   private
