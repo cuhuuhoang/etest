0f8f1e189a80a02b5f2e335dc27da92e3b55d0a0
Cu Huu Hoang <huuhoangcu@gmail.com>
2016-05-18 22:01:59 +0700

    working2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
-  before_filter :authenticate_user!
+  before_filter :authenticate_user! , if: -> { controller_name != "pron" }
 
     protected
 
