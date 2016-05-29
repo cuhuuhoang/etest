35f2d600bcfa16063852e3cdecf945398d7e56f9
huuhoangcu <huuhoangcu@gmail.com>
2016-05-29 09:19:44 -0400

    w

--- sh/spro.sh
+++ sh/spro.sh
@@ -2,4 +2,4 @@ RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
 pkill -f unicorn
 unicorn_rails -E production -c config/unicorn.rb -D
-service nginx restart
+#service nginx restart
